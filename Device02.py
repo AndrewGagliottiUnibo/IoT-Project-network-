bc68f000f6f9594483a50423fe9d6c611082106b
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2021-07-03 00:48:28 +0200

    modified a dummy error

--- Device02.py
+++ Device02.py
@@ -8,7 +8,7 @@ Created on Fri Jul  2 23:01:15 2021
 import SupportFunctions as sf
 
 ip = '192.168.1.2'
-fileName = 'Measures01.txt'
+fileName = 'Measures02.txt'
 gateway_address = ('localhost', 10000)
 measures = sf.detectionsReader(ip, fileName)
 sf.gatewayConnection(gateway_address, measures)
