ab56f3165ab811b853ad046c56461a72955914e9
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2021-07-03 21:25:39 +0200

    Problem was a wrong port number on this files

---
+++ Device04.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  2 23:01:18 2021
+
+@author: andre
+"""
+
+import SupportFunctions as sf
+  
+# We are defining ip address, name of the related file, gateway address to 
+# connect with.
+# After this we start reading the file content and send it to the gateway
+ip = '192.168.1.4'
+fileName = 'Measures04.txt'
+gateway_address = ('localhost', 10003)
+buffer = 4096
+
+measures = sf.detectionsReader(ip, fileName)
+sf.gatewayConnection(gateway_address, measures, buffer)
