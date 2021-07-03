c7872d2fd6044378007bf7a697710c70817496ab
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2021-07-03 20:55:38 +0200

    created a raw launcher but I need to fix a problem into establishing UDP connection

---
+++ launcher.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  3 20:07:42 2021
+
+@author: andre
+"""
+
+import Devices.Device01 as dvc01
+import Devices.Device02 as dvc02
+import Devices.Device03 as dvc03
+import Devices.Device04 as dvc04
+import ServerAndGateway.gateway as SADg
+import ServerAndGateway.server as SADs
+import Functions
+
+
+dvc01.launch()
+dvc02.launch()
+dvc03.launch()
+dvc04.launch()
+
+SADg.launch()
+SADs.launch()
