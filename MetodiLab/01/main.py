339cac708017e891188b32196079543fb4571a7a
AndrewGagliottiUnibo <andrew.gagliotti@studio.unibo.it>
2023-03-08 23:52:03 +0100

    updated

--- MetodiLab/01/main.py
+++ MetodiLab/01/main.py
@@ -1,15 +1,40 @@
-# Esercizio 1
 import numpy
+import matplotlib.pyplot as plt
 
+
+# Funzioni sfruttate
+# Esercizio 6
+def myfunc(first, second):
+    if first * second < 1000:
+        print(first * second)
+    else:
+        print(first + second)
+
+
+# Esercizio 7
+def funcSumProd(inputList):
+    return sum(inputList), numpy.prod(inputList)
+
+
+def funcSumOrProd(inputListDiff, value):
+    if value:
+        return sum(inputListDiff)
+    else:
+        return numpy.prod(inputListDiff)
+
+
+# Esercizi
+# Esercizio 1
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = []
 
 for i in a:
-    if(i < 5):
+    if i < 5:
         print(i)
         b.append(i)
 
 print(b)
+
 
 # Esercizio 2
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
@@ -21,21 +46,24 @@ print(c)
 c = list(dict.fromkeys(c))
 print(c)
 
+
 # Esercizio 3
 z = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
-print(list(filter(lambda i: i % 2 == 0, z)))
+print(list(filter(lambda e: e % 2 == 0, z)))
 print([i for i in z if i % 2 == 0])
+
 
 # Esercizio 4
 for i in range(10):
-    if(i < 6):
+    if i < 6:
         print('* ' * i)
     else:
         print('* ' * (10 - i))
 
+
 # Esercizio 5
-s1='by'
-s2='blueberry'
+s1 = 'by'
+s2 = 'blueberry'
 
 for i in range(len(s1)):
     if s2.count(s1[i]) > 0:
@@ -43,35 +71,40 @@ for i in range(len(s1)):
     else:
         print('false')
 
-# Esercizio 6
-def myfunc(x, y):
-    if x * y < 1000:
-        print(x * y)
-    else:
-        print(x + y)
 
+# Esercizio 6
 myfunc(3, 4)
 myfunc(50, 70)
 
+
 # Esercizio 7
-def funcSumProd(x):
-    return (sum(x), numpy.prod(x))
-
-def funcSumOrProd(x, value):
-    if value == True:
-        return sum(x)
-    else:
-        return numpy.prod(x)
-
-
 a = [1, 1, 2, 3, 5, 8, 89]
 print(funcSumProd(a))
 print(funcSumOrProd(a, True))
 print(funcSumOrProd(a, False))
 
+
 # Esercizio 8
-x = [0,1,2,3,4,5,6,7,8,9]
-y = [i**2 for i in x]
+x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+y = [i ** 2 for i in x]
+z = [i ** 3 for i in x]
+# oppure: y = list(filter(lambda i: i**2, x))
 print(y)
 
-# continua ...
+# Disegno i grafici
+plt.subplot(1, 2, 1)
+plt.title('y su x')
+plt.plot(y, x, 'ro-', linewidth=0.7)
+plt.plot(x, y, 'gd--', linewidth=0.7)
+plt.grid(axis = 'y')
+plt.legend(["Funzione x","Funzione y"])
+
+plt.subplot(1, 2, 2)
+plt.title('z su x')
+plt.plot(y, x, 'ro-', linewidth=0.7)
+plt.plot(z, y, 'bd-', linewidth=0.7)
+plt.grid(axis = 'y')
+plt.legend(["Funzione x","Funzione z"])
+
+plt.savefig("filename.jpg")  
+plt.show()
